d8abf028cf68951cbbd78f443893992a3e9c0d3b
Ravi Agrawal <agrawalravi95@gmail.com>
2016-01-13 02:39:59 +0545

    Updated Portfolio Section and fixed several bugs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,67 +1,70 @@
-//Jquery For accessible Tabs
-// a temp value to cache *what* we're about to show
-var target = null;
+/* function tabs() {
 
-// collect all the tabs
-var tabs = $('.tab').on('click', function () {
-  target = $(this.hash).removeAttr('id');
+  //Jquery For accessible Tabs
+  // a temp value to cache *what* we're about to show
+  var target = null;
 
-  // if the URL isn't going to change, then hashchange
-  // event doesn't fire, so we trigger the update manually
-  if (location.hash === this.hash) {
-    // but this has to happen after the DOM update has
-    // completed, so we wrap it in a setTimeout 0
-    setTimeout(update, 0);
+  // collect all the tabs
+  var tabs = $('.tab').on('click', function () {
+    target = $(this.hash).removeAttr('id');
+
+    // if the URL isn't going to change, then hashchange
+    // event doesn't fire, so we trigger the update manually
+    if (location.hash === this.hash) {
+      // but this has to happen after the DOM update has
+      // completed, so we wrap it in a setTimeout 0
+      setTimeout(update, 0);
+    }
+  });
+
+  // get an array of the panel ids (from the anchor hash)
+  var targets = tabs.map(function () {
+    return this.hash;
+  }).get();
+
+  // use those ids to get a jQuery collection of panels
+  var panels = $(targets.join(',')).each(function () {
+    // keep a copy of what the original el.id was
+    $(this).data('old-id', this.id);
+  });
+
+  function update() {
+    if (target) {
+      target.attr('id', target.data('old-id'));
+      target = null;
+    }
+
+    var hash = window.location.hash;
+    if (targets.indexOf(hash) !== -1) {
+      show(hash);
+    }
   }
-});
 
-// get an array of the panel ids (from the anchor hash)
-var targets = tabs.map(function () {
-  return this.hash;
-}).get();
+  function show(id) {
+    // if no value was given, let's take the first panel
+    if (!id) {
+      id = targets[0];
+    }
+    // remove the selected class from the tabs,
+    // and add it back to the one the user selected
+    tabs.removeClass('selected').filter(function () {
+      return (this.hash === id);
+    }).addClass('selected');
 
-// use those ids to get a jQuery collection of panels
-var panels = $(targets.join(',')).each(function () {
-  // keep a copy of what the original el.id was
-  $(this).data('old-id', this.id);
-});
-
-function update() {
-  if (target) {
-    target.attr('id', target.data('old-id'));
-    target = null;
+    // now hide all the panels, then filter to
+    // the one we're interested in, and show it
+    panels.hide().filter(id).show();
   }
 
-  var hash = window.location.hash;
-  if (targets.indexOf(hash) !== -1) {
-    show(hash);
+  $(window).on('hashchange', update);
+
+  // initialise
+  if (targets.indexOf(window.location.hash) !== -1) {
+    update();
+  } else {
+    show();
   }
-}
-
-function show(id) {
-  // if no value was given, let's take the first panel
-  if (!id) {
-    id = targets[0];
-  }
-  // remove the selected class from the tabs,
-  // and add it back to the one the user selected
-  tabs.removeClass('selected').filter(function () {
-    return (this.hash === id);
-  }).addClass('selected');
-
-  // now hide all the panels, then filter to
-  // the one we're interested in, and show it
-  panels.hide().filter(id).show();
-}
-
-$(window).on('hashchange', update);
-
-// initialise
-if (targets.indexOf(window.location.hash) !== -1) {
-  update();
-} else {
-  show();
-}
+} */
 
 
 
@@ -79,14 +82,70 @@ $("#home").mousemove(function(e){
           $('#home').css("background-position", newvalueX+"px     "+newvalueY+"px");
 });
 }); */
-var i = 0;
 
 $('#process__toggle').on('click', function() {
   
   $('.process__caption').toggle();
   $('.about__travel').toggleClass('process__details--open');
-  $('#portfolio-title').toggleClass('process__details--open');
   var text = $(this).text();
   $(this).text(text == '[ Hide Details ]' ? '[ View Details ]' : '[ Hide Details ]');
 
-})
+});
+
+
+// Slider 
+var arrow = "portfolio__arrow",
+    right = "portfolio__arrow--right",
+    left = "portfolio__arrow--left",
+    close = "portfolio-block--close",
+    open = "portfolio-block--open",
+    array = $('.portfolio-block'),
+    active = array.siblings("." + open);
+
+$('.portfolio__arrow').on('click', function() {
+
+  var block = $(this).parent();
+  var index = array.index(block);
+  
+  if( index == 1 ) {
+    $(this).toggleClass(left);
+    $(this).toggleClass(right);
+  }
+
+  if(array.index(active) == 1 && index == 0) {
+    active.children('.' + arrow).addClass(left);
+    active.children('.' + arrow).removeClass(right);
+  }
+
+  if(array.index(active) == 1 && index == 2) {
+    active.children('.' + arrow).addClass(right);
+    active.children('.' + arrow).removeClass(left);
+  }
+
+  if(array.index(active) == 0 && index == 2) {
+    array.eq(1).children('.' + arrow).addClass(right);
+    array.eq(1).children('.' + arrow).removeClass(left);
+  }
+
+  if(array.index(active) == 2 && index == 0) {
+    array.eq(1).children('.' + arrow).addClass(left);
+    array.eq(1).children('.' + arrow).removeClass(right);
+  }
+
+  active.addClass(close);
+  active.removeClass(open);
+  active.children('.portfolio__description').removeClass('visible');
+  active.children('.portfolio__image').removeClass('visible');
+
+  $(this).parent().addClass(open);
+  $(this).parent().removeClass(close);
+
+  setTimeout(function() {
+    block.children('.portfolio__description').addClass('visible');
+    block.children('.portfolio__image').addClass('visible');
+  }, 200);
+
+  active = array.siblings("." + open);
+  
+
+});
